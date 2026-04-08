982a4b9d63e58af87260244f805e26957ed40f09
Kiriosx1 <kyrosmpaltak@gmail.com>
2026-04-08 09:14:57 +0300

    Fix React Router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router>
+        <Router basename="/Portofolio">
           <NavigationTracker />
           <AuthenticatedApp />
         </Router>
